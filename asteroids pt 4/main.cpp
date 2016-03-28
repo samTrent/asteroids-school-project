d9fcc343377cec512e67ad7dd41f643f347cb261
Sam Trent <sd.trent@yahoo.com>
2016-03-28 17:13:29 -0600

    Initial Commit

---
+++ asteroids pt 4/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  asteroids pt 4
+//
+//  Created by Sam Trent on 3/28/16.
+//  Copyright © 2016 Sam Trent. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+   // insert code here...
+   std::cout << "Hello, World!\n";
+    return 0;
+}
